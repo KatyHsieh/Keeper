defe2395fd0fcf41638bbdce29bd200dd5f6c637
Katy <cat0420cat@gmail.com>
2022-09-08 16:03:31 +0800

    Updates

---
+++ precache-manifest.51bd6cbd68ed2940c56992d4d3cdc4e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e370db15dbc8b8509cb3c6cd85c0ae1",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "9dbf12dd3fdae1d2cc4b",
+    "url": "/Keeper/static/js/2.4b42c501.chunk.js"
+  },
+  {
+    "revision": "03f8f18465f401aadecf",
+    "url": "/Keeper/static/js/main.c41bc558.chunk.js"
+  },
+  {
+    "revision": "c2cd02bec1164cebc775",
+    "url": "/Keeper/static/js/runtime-main.8d08ef39.js"
+  }
+]);
